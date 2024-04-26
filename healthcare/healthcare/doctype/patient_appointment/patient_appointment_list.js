ee62f703988d055c34f599a7c31c3bfba3cc6fa8
isfaaq <isfaaqg@gmail.com>
2024-04-26 11:07:02 +0400

    basic consultation flow

--- healthcare/healthcare/doctype/patient_appointment/patient_appointment_list.js
+++ healthcare/healthcare/doctype/patient_appointment/patient_appointment_list.js
@@ -1,9 +1,11 @@
 /*
 (c) ESS 2015-16
 */
+const d = (((new Date()).toISOString().split('T')[0]).split('-').reverse()).join('-')
+
 frappe.listview_settings['Patient Appointment'] = {
-	filters: [["status", "=", "Open"]],
-	get_indicator: function(doc) {
+	filters: [],
+	get_indicator: function (doc) {
 		var colors = {
 			"Open": "orange",
 			"Scheduled": "yellow",
@@ -14,5 +16,23 @@ frappe.listview_settings['Patient Appointment'] = {
 			"Checked Out": "orange"
 		};
 		return [__(doc.status), colors[doc.status], "status,=," + doc.status];
-	}
+	},
+	button: {
+		show() {
+			const is_doctor = frappe.user.has_role('Doctor')
+			return is_doctor;
+		},
+		get_description() { },
+		get_label() {
+			return 'Begin Consultation';
+		},
+		action(doc) {
+			// when clicking this button should open new consultation with prefilled values
+
+			// const link = '/app/patient-encounter/new-patient-encounter' + '/?' + 'patient=' + encodeURIComponent(doc.patient);
+			// frappe.set_route(link);
+
+			frappe.set_route("Form", "Patient Encounter", "new-patient-encounter");
+		}
+	},
 };

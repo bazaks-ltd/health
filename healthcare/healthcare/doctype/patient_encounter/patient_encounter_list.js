787086ef5914277643247210f35321abb2e2bf0a
Terence Adrien Zama <terence.zama@gmail.com>
2025-02-06 10:24:14 +0400

    healthcare changes

--- healthcare/healthcare/doctype/patient_encounter/patient_encounter_list.js
+++ healthcare/healthcare/doctype/patient_encounter/patient_encounter_list.js
@@ -1,6 +1,9 @@
 /*
 (c) ESS 2015-16
 */
-frappe.listview_settings['Patient Encounter'] = {
-	filters:[["docstatus","!=","2"]]
+frappe.listview_settings["Patient Encounter"] = {
+  filters: [["docstatus", "!=", "2"]],
+  hide_name_column: true,
+  hide_title_column: true,
+  hide_name_filter: true,
 };
